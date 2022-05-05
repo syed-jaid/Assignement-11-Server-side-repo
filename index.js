081e2303a462a21f4d860ee5561c1d7ce483b205
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-05-05 19:39:10 +0600

    get data from data base

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
 const app = express()
+require('dotenv').config()
 const port = 5000
 
 
+app.use(express.json())
+app.use(cors())
+
+
+
+const uri = "mongodb+srv://database:GS1wVWfYiLdkHHVM@cluster0.h6zfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('Allcards').collection('cards')
+        // get data form database
+        app.get('/cards', async (req, res) => {
+            const query = {}
+            const cursor = database.find(query)
+            const result = await cursor.toArray()
+            console.log(result)
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
