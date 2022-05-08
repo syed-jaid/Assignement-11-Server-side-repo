118acfd11507743210ceb517cbd0f7c24d9a1140
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-05-08 23:03:04 +0600

    the

--- index.js
+++ index.js
@@ -82,7 +82,9 @@ async function run() {
     }
 }
 run().catch(console.dir);
-
+app.get('/', (req, res) => {
+    res.send('data')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
